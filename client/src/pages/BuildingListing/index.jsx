9761012bc275be859fb5cecc3b021c3d713b718f
Briarwoodsz <johnjosephhoy@gmail.com>
2020-01-09 19:19:12 -0700

    created base gallery page for the building listing and an error page
    the buidling listing just has a base concept of what fields are needed and where they will be placed, and the error page will just be displayed for when users visit an address on our site that we do not actual have.

--- client/src/pages/BuildingListing/index.jsx
+++ client/src/pages/BuildingListing/index.jsx
@@ -1,6 +1,38 @@
 import React, { Component } from 'react';
-
+import { NavBar } from '../../components/Navbar';
+import { Header } from '../../components/Header';
 
 export default class Listing extends Component {
-     
+     constructor(props) {
+          super(props)
+          this.state = {
+               user: props.user,
+               styleProp: 'absolute'
+          }
+     }
+
+     componentDidMount() {
+          
+     }
+
+     render() {
+          if (this.props.user) {
+               return (
+                    <div className="listing">
+                         <NavBar styleProp={this.state.styleProp} />
+                         <Header user={this.state.user} />
+                         <h1> Gallery </h1>
+                    </div>
+               )
+          } else {
+               return (
+                    <div className="listing">
+                         <NavBar styleProp={this.state.styleProp} />
+                         <Header user={this.state.user} />
+                         <h1> Gallery </h1>
+                    </div>
+               )
+          }
+     }
+
 }
