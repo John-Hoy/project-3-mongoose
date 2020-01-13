794861515452ce2239ef876bb07cad0accb5fb06
William Hall <billhall168@gmail.com>
2020-01-13 11:44:59 -0700

    added some breaks to display form better, and it now alerts the js object input

--- client/src/components/AddTenant/index.jsx
+++ client/src/components/AddTenant/index.jsx
@@ -5,7 +5,7 @@ import * as Yup from 'yup';
 const AddTenant = () => {
   return (
     <Formik
-      initialValues={{ first_name: '', last_name: '', unit_no: '', rent_current: '', rent_starting: '', rent_last_paid: '', rent_status: '', issues: [], email: '' }}
+      initialValues={{ first_name: '', last_name: '', unit_no: '', rent_current: '', rent_starting: '', rent_last_paid: '', rent_status: '', issues: ''}}
       validationSchema={Yup.object({
         first_name: Yup.string()
           .max(15, 'Must be 15 characters or less')
@@ -13,13 +13,13 @@ const AddTenant = () => {
         last_name: Yup.string()
           .max(20, 'Must be 20 characters or less')
           .required('Required'),
-        unit_no: Yup.number()
-          .max(4, 'Must be 20 characters or less')
-          .required('Required'),
-        rent_current: Yup.number()
+        unit_no: Yup.string()
           .max(20, 'Must be 20 characters or less')
           .required('Required'),
-        rent_starting: Yup.number()
+        rent_current: Yup.string()
+          .max(20, 'Must be 20 characters or less')
+          .required('Required'),
+        rent_starting: Yup.string()
           .max(20, 'Must be 20 characters or less')
           .required('Required'),
         rent_last_paid: Yup.string()
@@ -46,30 +46,39 @@ const AddTenant = () => {
         <label htmlFor="first_name">First Name</label>
         <Field name="first_name" type="text" />
         <ErrorMessage name="first_name" />
+        <br />
         <label htmlFor="last_name">Last Name</label>
         <Field name="last_name" type="text" />
         <ErrorMessage name="last_name" />
+        <br />
         <label htmlFor="unit_no">Unit Number:</label>
         <Field name="unit_no" type="number" />
         <ErrorMessage name="unit_no" />
+        <br />
         <label htmlFor="rent_current">Current Rent:</label>
         <Field name="rent_current" type="number" />
         <ErrorMessage name="unit_no" />
+        <br />
         <label htmlFor="rent_starting">Starting Rent:</label>
         <Field name="rent_starting" type="number" />
         <ErrorMessage name="rent_starting" />
+        <br />
         <label htmlFor="rent_last_paid">Rent Last Paid?</label>
         <Field name="rent_last_paid" type="text" />
         <ErrorMessage name="rent_last_paid" />
+        <br />
         <label htmlFor="rent_status">Rent Status:</label>
         <Field name="rent_status" type="text" />
         <ErrorMessage name="rent_status" />
+        <br />
         <label htmlFor="issues">Issues:</label>
         <Field name="issues" type="text" />
         <ErrorMessage name="issues" />
+        <br />
         <label htmlFor="mgr_comments">Manager Comments:</label>
         <Field name="mgr_comments" type="textarea" />
         <ErrorMessage name="mgr_comments" />
+        <br />
         
         <button type="submit">Submit</button>
       </Form>
