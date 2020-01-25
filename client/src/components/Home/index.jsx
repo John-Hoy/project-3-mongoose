ec403e706d33493380289081923631a22063c8e6
William Hall <billhall168@gmail.com>
2020-01-25 11:50:01 -0700

    some links added on resources page

--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -20,7 +20,7 @@ const HomeComponent = () => {
                            <img src="images/shack.png" alt="shack" />
                         </div>
                         <div className="column is-three-quarter">
-                            <p>Welcome to the homepage of our app. From here, you can <a href="/ManageTenants"> manage tenants</a>, <a href="/">add properties</a>, <a href="/Contact">contact us</a>, or find <a href="/Resources"> valuable resources.</a> If you are a tenant, you can let your landord know of any <a href="/Issues">issues</a> you may have, download a <a href="#"> rental agreement form</a> or find relevant information regarding rental laws in your state.</p>
+                            <p>Welcome to the homepage of our app. From here, you can <a href="/ManageTenants"> manage tenants</a>, <a href="/">add properties</a>, <a href="/Contact">contact us</a>, or find <a href="/Resources"> valuable resources.</a> If you are a tenant, you can let your landord know of any <a href="/Issues">issues</a> you may have, download a <a href="/lease"> rental agreement form</a> or find relevant information regarding rental laws in your state.</p>
                         </div>
                     </div>
                 </div>
