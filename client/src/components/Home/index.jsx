8d964a5800c7da69e77f9e1c8e9ce6cc3f6f8d2c
William Hall <billhall168@gmail.com>
2020-01-25 07:09:47 -0700

    updated home page text and links

--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -20,7 +20,7 @@ const HomeComponent = () => {
                            <img src="images/shack.png" alt="shack" />
                         </div>
                         <div className="column is-three-quarter">
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                            <p>Welcome to the homepage of our app. From here, you can <a href="/ManageTenants"> manage tenants</a>, <a href="/">add properties</a>, <a href="/Contact">contact us</a>, or find <a href="/Resources"> valuable resources.</a> If you are a tenant, you can let your landord know of any <a href="/Issues">issues</a> you may have, download a <a href="#"> rental agreement form</a> or find relevant information regarding rental laws in your state.</p>
                         </div>
                     </div>
                 </div>
